651c9021538c797b6a6351408df48842961b8405
Jim Barritt <jimb@thoughtworks.com>
2018-03-24 04:49:57 +0000

    Adding skeleton main

---
+++ src/main/java/jmdb/tutorial/nettyio/SocksServer.java
@@ -0,0 +1,13 @@
+package jmdb.tutorial.nettyio;
+
+/**
+ * A simple server that responds to calls over the SOCKS protocol
+ */
+public class SocksServer {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Socks server...");
+    }
+
+
+}
